4b716d1f60c9028c0a6a0a84d2a8a9858d92afda
Luna Fernandez <Luna.Fernandez@anniecannonss-MacBook-Pro.local>
2024-07-14 03:12:03 -0600

    second commit

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
 import projectData from "../data/data.js"
 
 console.log(projectData)
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector(".header-burger i");
+    const nav = document.querySelector(".bar-nav");
+  
+    function showHamburger() {
+      burger.classList.toggle("fa-times");
+      nav.classList.toggle("nav-active");
+      console.log("showHamburger");
+    }
+  
+    document.getElementById('hamburgerButton').addEventListener('click', showHamburger);
+  });
