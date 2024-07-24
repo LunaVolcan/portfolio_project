d3144cf8bd54a09451b3f9ce132b2839f9a0eed7
Luna Fernandez <Luna.Fernandez@anniecannonss-MacBook-Pro.local>
2024-07-24 12:52:57 -0600

    complete

--- js/custom.js
+++ js/custom.js
@@ -4,17 +4,17 @@ import projectData from "../data/data.js"
 // hamburger
 
 document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector(".header-burger i");
-    const nav = document.querySelector(".bar-nav");
-  
-    function showHamburger() {
-      burger.classList.toggle("fa-times");
-      nav.classList.toggle("nav-active");
-      console.log("showHamburger");
-    }
-  
-    document.getElementById('hamburgerButton').addEventListener('click', showHamburger);
-  });
+  const burger = document.querySelector(".header-burger i");
+  const nav = document.querySelector(".bar-nav");
+
+  function showHamburger() {
+    burger.classList.toggle("fa-times");
+    nav.classList.toggle("nav-active");
+    console.log("showHamburger");
+  }
+
+  document.getElementById('hamburgerButton').addEventListener('click', showHamburger);
+});
 
 
 // projects
@@ -27,36 +27,36 @@ function logProjects() {
 
     for (let i = 0; i < projects.length; i++) {
       const project = projects[i];
-  
+
       const card = document.createElement('div');
       card.classList.add('card');
-  
+
       const title = document.createElement('h3');
       title.classList.add('project-text');
       title.textContent = project.title;
-  
+
       const img = document.createElement('img');
       img.src = project.image;
       img.classList.add('project-imgs');
-  
+
       const description = document.createElement('p');
       description.classList.add('project-p');
       description.textContent = project.description;
-  
+
       const link = document.createElement('a');
       link.href = project.link;
       link.target = '_blank';
       link.classList.add('button');
       link.textContent = 'View The Site';
-  
+
       const linkParagraph = document.createElement('p');
       linkParagraph.appendChild(link);
-  
+
       card.appendChild(title);
       card.appendChild(img);
       card.appendChild(description);
       card.appendChild(linkParagraph);
-  
+
       projectSection.appendChild(card);
     }
   }
@@ -73,15 +73,15 @@ let userArray = [];
 if (form) {
 
   form.addEventListener("submit", onFormSubmit);
-  
+
   function onFormSubmit(event) {
-      event.preventDefault();
-      const data = new FormData(event.target);
-      const dataObject = Object.fromEntries(data.entries());
-  
-      userArray.push(dataObject);
-      console.log(userArray);
-      form.reset();
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const dataObject = Object.fromEntries(data.entries());
+
+    userArray.push(dataObject);
+    console.log(userArray);
+    form.reset();
   }
 }
 
@@ -94,38 +94,38 @@ const madLibForm = document.getElementById("madLibForm");
 const madLibResult = document.getElementById("madLibResult");
 
 if (madLibPopup && madLibBtn) {
-    madLibBtn.addEventListener("click", toggleMadLib);
+  madLibBtn.addEventListener("click", toggleMadLib);
 
-    function toggleMadLib() {
-        madLibPopup.classList.toggle("hidden");
-        console.log("Toggle Madlib Popup");
-    }
+  function toggleMadLib() {
+    madLibPopup.classList.toggle("hidden");
+    console.log("Toggle Madlib Popup");
+  }
 }
 
 if (madLibForm) {
-    madLibForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        generateMadLib();
-        resetForm();
-    });
+  madLibForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    generateMadLib();
+    resetForm();
+  });
 
-    function generateMadLib() {
-        const noun = document.getElementById("noun").value;
-        const verb = document.getElementById("verb").value;
-        const adjective = document.getElementById("adjective").value;
-        const adverb = document.getElementById("adverb").value;
+  function generateMadLib() {
+    const noun = document.getElementById("noun").value;
+    const verb = document.getElementById("verb").value;
+    const adjective = document.getElementById("adjective").value;
+    const adverb = document.getElementById("adverb").value;
 
-        const madLibSentence = `The ${noun} ${verb} through the ${adjective}  system ${adverb}, causing a major outage.`
+    const madLibSentence = `The ${noun} ${verb} through the ${adjective}  system ${adverb}, causing a major outage.`
 
-        madLibResult.textContent = madLibSentence;
-        console.log("Mad Lib Created:", madLibSentence);
-    }
+    madLibResult.textContent = madLibSentence;
+    console.log("Mad Lib Created:", madLibSentence);
+  }
 
-    function resetForm() {
-        madLibForm.reset();
-        console.log("Form reset");
-    }
- }
+  function resetForm() {
+    madLibForm.reset();
+    console.log("Form reset");
+  }
+}
 
 // Color picker 
 
@@ -138,21 +138,21 @@ const colorSelect = document.getElementById("colorSelect");
 const changeTypeSelect = document.getElementById("changeTypeSelect");
 
 if (colorPickerButton && colorPickerForm && colorLabel && changeTypeLabel && applyColorButton && colorSelect && changeTypeSelect) {
-    colorPickerButton.addEventListener("click", function() {
-        colorPickerForm.classList.toggle("form-hide");
-        colorPickerForm.classList.toggle("form-show");
-    });
+  colorPickerButton.addEventListener("click", function () {
+    colorPickerForm.classList.toggle("form-hide");
+    colorPickerForm.classList.toggle("form-show");
+  });
 
-    applyColorButton.addEventListener("click", function() {
-        const selectedColor = colorSelect.value;
-        const changeType = changeTypeSelect.value;
+  applyColorButton.addEventListener("click", function () {
+    const selectedColor = colorSelect.value;
+    const changeType = changeTypeSelect.value;
 
-        if (changeType === "text") {
-            colorLabel.style.color = selectedColor;
-            changeTypeLabel.style.color = selectedColor;
-        } else if (changeType === "background") {
-            colorPickerForm.style.backgroundColor = selectedColor;
-        }
-    });
+    if (changeType === "text") {
+      colorLabel.style.color = selectedColor;
+      changeTypeLabel.style.color = selectedColor;
+    } else if (changeType === "background") {
+      colorPickerForm.style.backgroundColor = selectedColor;
+    }
+  });
 }
 
