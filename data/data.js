94f9f44818883e9cb2d7b3b467e66ba50c173fe4
Luna Fernandez <Luna.Fernandez@anniecannonss-MacBook-Pro.local>
2024-07-24 13:11:07 -0600

    js bug fix

--- data/data.js
+++ data/data.js
@@ -1,25 +1,25 @@
 const projectData = [
     {
         title: "Cannons Coffee",
-        image: "img/portfolio-coffee.png",
-        description: "Cannons Coffee was my first major project. I began by working with images of the site at large and medium dimensions, and then I coded the necessary HTML and CSS to create two pages, utilizing CSS Grid for the layouts. As I progressed in my learning journey, I incorporated a hamburger menu for mobile dimensions using JavaScript.",
-        link: "https://lunavolcan.github.io/cannons-coffee/",
+         image: "img/portfolio-coffee.png",
+         description: "Cannons Coffee was my first major project. I began by working with images of the site at large and medium dimensions, and then I coded the necessary HTML and CSS to create two pages, utilizing CSS Grid for the layouts. As I progressed in my learning journey, I incorporated a hamburger menu for mobile dimensions using JavaScript.",
+         link: "https://lunavolcan.github.io/cannons-coffee/",
     },
     {
         title: "Ice Cream Form",
-        image: "img/portfolio-icecream.png",
-        description: "Through the Ice Cream Ordering Form project, I gained proficiency in HTML form elements, CSS styling, and form submission using JavaScript processing. This form features a variety of input types, including text fields, text areas, radio buttons, checkboxes, and a dropdown menu.",
-        link: "https://lunavolcan.github.io/ice-cream-form/",
+         image: "img/portfolio-icecream.png",
+         description: "Through the Ice Cream Ordering Form project, I gained proficiency in HTML form elements, CSS styling, and form submission using JavaScript processing. This form features a variety of input types, including text fields, text areas, radio buttons, checkboxes, and a dropdown menu.",
+         link: "https://lunavolcan.github.io/ice-cream-form/",
     },
     {
         title: "Farmers Market",
-        image: "img/portfolio-fm.png",
-        description: "Applying the knowledge gained from the Cannons Coffee project, I constructed this website based on the provided specifications. Additionally, I enhanced it with JavaScript elements, including a hamburger button.",
-        link: "https://lunavolcan.github.io/farmers_market/",
+         image: "img/portfolio-fm.png",
+         description: "Applying the knowledge gained from the Cannons Coffee project, I constructed this website based on the provided specifications. Additionally, I enhanced it with JavaScript elements, including a hamburger button.",
+        link: "https://lunavolcan.github.io/farmers_market/", 
     },
 ]
 
 
 export default projectData
-git 
+
 
